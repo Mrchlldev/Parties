4a618a2700bf4b8e5888cc67a7642379076cd4ee
Missouriii <105463867+Missouriii@users.noreply.github.com>
2022-08-28 13:19:55 -0400

    Update PartyMenuForm.php

--- src/diduhless/parties/form/PartyMenuForm.php
+++ src/diduhless/parties/form/PartyMenuForm.php
@@ -18,7 +18,7 @@ class PartyMenuForm extends SimpleForm {
 
     public function __construct(Session $session) {
         $this->session = $session;
-        parent::__construct("Party Menu", "You do not have a party! Create a party or accept an invitation to join a party.");
+        parent::__construct("§l§6Party Menu", "§cYou do not have a party! Create a party or accept an invitation to join a party.");
     }
 
     protected function onCreation(): void {
@@ -28,7 +28,7 @@ class PartyMenuForm extends SimpleForm {
     }
 
     private function addCreatePartyButton(): void {
-        $button = new Button("Create a party");
+        $button = new Button("§aCreate a party");
         $button->setSubmitListener(function(Player $player) {
             $party = new Party(uniqid(), $this->session);
             $event = new PartyCreateEvent($party, $this->session);
@@ -44,13 +44,13 @@ class PartyMenuForm extends SimpleForm {
     }
 
     public function addPublicPartiesButton(): void {
-        $this->addButton(new Button("Join a public party", null, function(Player $player) {
+        $this->addButton(new Button("§2Join a public party", null, function(Player $player) {
             $player->sendForm(new PublicPartiesForm($this->session));
         }));
     }
 
     public function addInvitationsButton(): void {
-        $this->addButton(new Button("View invitations [" . count($this->session->getInvitations()) . "]", null, function(Player $player) {
+        $this->addButton(new Button("§eView invitations [" . count($this->session->getInvitations()) . "]", null, function(Player $player) {
             $player->sendForm(new InvitationsForm($this->session));
         }));
     }
