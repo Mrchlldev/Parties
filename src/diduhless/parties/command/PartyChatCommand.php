de1e80e5276b84fd2a1ad9bf72897168e438b638
Missouriii <105463867+Missouriii@users.noreply.github.com>
2022-09-01 16:52:23 -0400

    Update PartyChatCommand.php

--- src/diduhless/parties/command/PartyChatCommand.php
+++ src/diduhless/parties/command/PartyChatCommand.php
@@ -15,15 +15,15 @@ class PartyChatCommand extends SessionCommand {
     public function onCommand(Session $session, array $args) {
         $party_chat = $session->hasPartyChat();
         if(!$session->hasParty()) {
-            $session->message("§l§6» §r§cYou must be in a party to do this!");
+            $session->message(" §l§6» §r§cYou must be in a party to do this!");
             return;
         }
 
         $session->setPartyChat(!$party_chat);
         if($party_chat) {
-            $session->message("§l§6»§r§a You have disabled party chat.");
+            $session->message(" §l§6»§r§a You have disabled party chat.");
         } else {
-            $session->message("§l§6»§r§a You are now in party chat!");
+            $session->message(" §l§6»§r§a You are now in party chat!");
         }
     }
 
